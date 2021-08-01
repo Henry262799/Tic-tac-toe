c3c20c0f880a5e3dd92ab15658a20199b1f379df
henry <henrychen262799@gmail.com>
2021-08-01 19:41:19 -0400

    Implemented minimax algorithm and dumbified it for different computer difficulties

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const player=(sign)=>{
 
 const computerPlayer= (sign)=>{
 
+
+    //This method returns the availables spots the computer can place their move 
     let computerChoice= ()=>{
 
         let choices=new Array(9);
@@ -32,6 +34,7 @@ const computerPlayer= (sign)=>{
         return availableChoices; 
     }
 
+    // This method chooses a random place from the available spots to place the computers choice
     let computerPlaceLocation= ()=>{
 
         let choiceArrayRef=computerChoice();
@@ -41,27 +44,29 @@ const computerPlayer= (sign)=>{
         //return choiceArrayRef.length;
     }
 
-
-    let scores={
+    // This object is a helper to return the scores for the minimax algorithm and dictates the optimal choice for the AI
+    let _scores={
         computer:10,
         player:-10,
         tie:0
     }
 
-    let miniMax=(board,depth,isMaxmizing)=>{
+
+    // This is the main algorithm that gives us the best choice for the AI player.
+    let _miniMax=(board,depth,isMaxmizing)=>{
 
         if(gameController.checkPlayerWin()){
 
-            return scores.player;
+            return _scores.player;
 
         }
         else if(gameController.checkComputerWin()){
             
-            return scores.computer;
+            return _scores.computer;
         }
         else if(gameController.isGameOver()){
 
-            return scores.tie;
+            return _scores.tie;
         }
 
 
@@ -71,7 +76,7 @@ const computerPlayer= (sign)=>{
             for(let i=0;i<gameBoard.getGameBoard().length;i++){
                 if(gameBoard.getGameBoard()[i]==undefined){
                     gameBoard.setGameBoard(i,sign);
-                    let score=miniMax(gameBoard.getGameBoard(),depth+1,false);
+                    let score=_miniMax(gameBoard.getGameBoard(),depth+1,false);
                     score=score+depth;
                     gameBoard.setGameBoard(i,undefined);
                     if(score>scoreBest){
@@ -89,7 +94,7 @@ const computerPlayer= (sign)=>{
             for(let i=0;i<gameBoard.getGameBoard().length;i++){
                 if(gameBoard.getGameBoard()[i]==undefined){
                     gameBoard.setGameBoard(i,gameController.getHumanPlayer().sign);
-                    let score=miniMax(gameBoard.getGameBoard(),depth+1,true);
+                    let score=_miniMax(gameBoard.getGameBoard(),depth+1,true);
                      score=score-depth;
                     gameBoard.setGameBoard(i,undefined);
                     if(score<scoreBest){
@@ -104,6 +109,7 @@ const computerPlayer= (sign)=>{
 
     }
 
+    // Returns the bestmove for the ai player.
     let bestMove=()=>{
 
         let moveBest;
@@ -111,7 +117,7 @@ const computerPlayer= (sign)=>{
         for(let i=0;i<gameBoard.getGameBoard().length;i++){
             if(gameBoard.getGameBoard()[i]==undefined){
                 gameBoard.setGameBoard(i,sign);
-                let score=miniMax(gameBoard.getGameBoard(),0,false);
+                let score=_miniMax(gameBoard.getGameBoard(),0,false);
                 gameBoard.setGameBoard(i,undefined);
                 if(score>bestScore){
                     bestScore=score;
@@ -147,23 +153,29 @@ const gameBoard= function(){
     //console.log(_gameBoard.length);
         
 
+    // This function returns a true or false value based on whether the board spot is taken or not.
     let isTaken=(number)=>{
 
         return _gameBoard[number]!==undefined;
     }
 
+    // This method returns the reset Button for the game.
     let getResetButton=()=>{
         return _resetButton;
     }
 
+    // This method returns the current gameBoard.
     let getGameBoard=()=>{
         return _gameBoard;
     }
 
+    // This method returns the playable boardButtons.
     let getBoardButtons=()=>{
         return _gameBoardButtons;
     }
 
+
+    // This method sets a specific location of the board.
     let setGameBoard=(number,value)=>{
 
         _gameBoard[number]=value;
@@ -195,24 +207,30 @@ const gameController= function(){
     let _ai=computerPlayer('O');
 
 
+    // Returns the ai player
     let getComputerPlayer=()=>{
 
         return _ai;
     }
 
+    // Returns the human player
     let getHumanPlayer=()=>{
         return _human_player;
     }
 
+    // Returns the players sign
     let getSign=()=>{
         return _human_player.sign;
     }
 
+    // Returns a boolean value whether the game is over or not
     let isGameOver=()=>{
 
         return checkPlayerWin() || checkComputerWin() || _isGridFull();
     }
 
+
+    // Returns a boolean value whether the gameboard grid is full or not.
     let _isGridFull=()=>{
 
         for(let i=0;i<gameBoard.getGameBoard().length;i++){
@@ -230,11 +248,15 @@ const gameController= function(){
         return true;
     }
 
+
+    // This procedure resets the entire board 
     let resetBoard=()=>{
 
         gameBoard.getGameBoard().fill(undefined);
     }
 
+
+    // This method returns a boolean checking whether the player has a row win.
     let _checkPlayerRowWin=()=>{
 
         let boardRef=gameBoard.getGameBoard();
@@ -253,6 +275,8 @@ const gameController= function(){
 
     }
 
+
+    // This method returns a boolean checking whether the player has a column win.
     let _checkPlayerColumnWin=()=>{
 
         let boardRef=gameBoard.getGameBoard();
@@ -270,6 +294,8 @@ const gameController= function(){
         return false;
     }
 
+
+    // This method returns a boolean whether the player has a diagonal win.
     let _checkPlayerDiagonalWin=()=>{
 
         let boardRef=gameBoard.getGameBoard();
@@ -285,6 +311,8 @@ const gameController= function(){
 
     }
 
+ 
+    // This method returns a boolean whether the computer has a Row win.
     let checkComputerRowWin= ()=>{
 
         let boardRef=gameBoard.getGameBoard();
@@ -303,6 +331,8 @@ const gameController= function(){
 
     }
 
+
+    // This method returns a boolean whether the computer has a column win.
     let checkComputerColumnWin= ()=>{
 
         let boardRef=gameBoard.getGameBoard();
@@ -322,6 +352,7 @@ const gameController= function(){
 
     }
 
+    // This method returns a boolean whether the computer has a diagonal win.
     let checkComputerDiagonalWin=()=>{
 
         let boardRef=gameBoard.getGameBoard();
@@ -337,11 +368,13 @@ const gameController= function(){
 
     }
 
+    // This method returns a boolean whether the player has won.
     let checkPlayerWin=()=>{
 
         return _checkPlayerRowWin() || _checkPlayerColumnWin() || _checkPlayerDiagonalWin();
     }
 
+    // This method returns a boolean whether the computer has won.
     let checkComputerWin=()=>{
 
         return checkComputerRowWin() || checkComputerColumnWin() || checkComputerDiagonalWin();
@@ -368,6 +401,20 @@ const gameController= function(){
 const displayControllar= function(){
 
 
+    // Document references.
+    let winner=document.querySelector('.winner');
+    let easyButton=document.querySelector('#easy');
+    let mediumButton=document.querySelector('#medium');
+    let hardButton=document.querySelector('#hard');
+    let impossibleButton=document.querySelector('#impossible');
+
+
+    // Set the difficulty
+    let difficulty='easy';
+
+
+
+    // Display the board 
     let displayBoard= ()=>{
 
         //console.log('here');
@@ -386,6 +433,7 @@ const displayControllar= function(){
     }
 
 
+    // This is an event listener for the playable buttons of the board.
     let _buttonEventListenerHelper=(event)=>{
         //console.log(event.target);
 
@@ -401,10 +449,12 @@ const displayControllar= function(){
 
     }
         
-
+    // This method resets the board.
     let resetBoard=()=>{
         gameController.resetBoard();
         displayBoard();
+        winner.textContent="";
+        winner.style.display='none';
         gameBoard.getBoardButtons().forEach(button=>{
             button.addEventListener('click',playGame);   
     
@@ -412,6 +462,8 @@ const displayControllar= function(){
 
     }
 
+
+    // This method plays the tic-tac-toe game.
     let playGame=(event)=>{
 
         if(gameController.isGameOver()){
@@ -423,6 +475,19 @@ const displayControllar= function(){
         event.target.removeEventListener('click',playGame);
 
         if(gameController.isGameOver()){
+            if(gameController.checkPlayerWin()){
+                winner.style.display="inline";
+                winner.textContent="Player Win";
+            }
+            else if(gameController.checkComputerWin()){
+                winner.style.display="inline";
+                winner.textContent="Computer Win"
+            }
+            else{
+                winner.style.display='inline';
+                winner.textContent="TIE";
+            }
+
             return;
         }
 
@@ -430,8 +495,27 @@ const displayControllar= function(){
         let computerPick=computerChoice();
         displayBoard();
 
+        if(gameController.isGameOver()){
+            if(gameController.checkPlayerWin()){
+                winner.style.display="inline";
+                winner.textContent="Player Win";
+            }
+            else if(gameController.checkComputerWin()){
+                winner.style.display="inline";
+                winner.textContent="Computer Win"
+            }
+            else{
+                winner.style.display="inline";
+                winner.textContent="TIE";
+            }
+
+            return;
+        }
+
     }
 
+
+    // This sets the players choice.
     let playerChoice=(e)=>{
 
         let trgt=e.target;
@@ -445,11 +529,39 @@ const displayControllar= function(){
 
     }
 
-
+    // This sets the computer choice.
     let computerChoice=()=>{
-
+        let pickLocation;
+        if(difficulty==='easy'){
+            //console.log('here');
+            pickLocation=gameController.getComputerPlayer().computerPlaceLocation();
+        }
+        else if(difficulty==='medium'){
+            //console.log('here2');
+            let random=Math.floor(Math.random()*10);
+            if(random<4){
+                pickLocation=gameController.getComputerPlayer().computerPlaceLocation();
+            }
+            else{
+                pickLocation=gameController.getComputerPlayer().bestMove();
+            }
+        }
+        else if(difficulty==='hard'){
+            //console.log('here3');
+            let random=Math.floor(Math.random()*10);
+            if(random<1){
+                pickLocation=gameController.getComputerPlayer().computerPlaceLocation();
+            }
+            else{
+                pickLocation=gameController.getComputerPlayer().bestMove();
+            }
+        }
+        else if(difficulty==='impossible'){
+            //console.log('here4');
+            pickLocation=gameController.getComputerPlayer().bestMove();
+        }
         //let pickLocation= gameController.getComputerPlayer().computerPlaceLocation();
-        let pickLocation=gameController.getComputerPlayer().bestMove();
+        //let pickLocation=gameController.getComputerPlayer().bestMove();
         console.log(pickLocation);
         let comp_sign=gameController.getComputerPlayer().sign;
         let button=gameBoard.getBoardButtons()[pickLocation];
@@ -457,8 +569,7 @@ const displayControllar= function(){
         button.textContent=comp_sign;
         button.removeEventListener('click',playGame);
         return button;
-
-        
+      
     }
 
 
@@ -467,6 +578,22 @@ const displayControllar= function(){
       gameBoard.getBoardButtons().forEach(button=>{
         button.addEventListener('click',playGame);   
 
+    })
+
+    easyButton.addEventListener('click', (e)=>{
+        difficulty='easy';
+    })
+
+    mediumButton.addEventListener('click', (e)=>{
+        difficulty='medium';
+    })
+
+    hardButton.addEventListener('click', (e)=>{
+        difficulty='hard';
+    })
+
+    impossibleButton.addEventListener('click',(e)=>{
+        difficulty='impossible';
     })
 
     gameBoard.getResetButton().addEventListener('click',resetBoard);
